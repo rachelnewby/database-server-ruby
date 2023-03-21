182a9988c23adbd6d1daea79958009ac6a87dd08
Rachel Newby <rachelcnewby@gmail.com>
2023-03-21 18:53:08 +0000

    When multiple key value pairs are added, they are both stored

--- spec/data_repo_spec.rb
+++ spec/data_repo_spec.rb
@@ -6,4 +6,11 @@ describe DataRepo do
     data_repo.set("first", "hello")
     expect(data_repo.instance_variable_get("@data")).to eq({"first" => "hello"})
   end
+
+  it "sets multiple data fields into the repo" do
+    data_repo = DataRepo.new
+    data_repo.set("first", "hello")
+    data_repo.set("second", "hi")
+    expect(data_repo.instance_variable_get("@data")).to eq({"first" => "hello", "second" => "hi"})
+  end
 end
