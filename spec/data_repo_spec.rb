4d080a5b1a5470bec6621ab740a7e4da493f8063
Rachel Newby <rachelcnewby@gmail.com>
2023-03-21 19:01:13 +0000

    Tests refactored: added before each to initialize the class

--- spec/data_repo_spec.rb
+++ spec/data_repo_spec.rb
@@ -1,22 +1,23 @@
 require 'data_repo'
 
 describe DataRepo do
+  before(:each) do
+    @data_repo = DataRepo.new
+  end
+
   it "sets data into the repo" do
-    data_repo = DataRepo.new
-    data_repo.set("first", "hello")
-    expect(data_repo.instance_variable_get("@data")).to eq({"first" => "hello"})
+    @data_repo.set("first", "hello")
+    expect(@data_repo.instance_variable_get("@data")).to eq({"first" => "hello"})
   end
 
   it "sets multiple data fields into the repo" do
-    data_repo = DataRepo.new
-    data_repo.set("first", "hello")
-    data_repo.set("second", "hi")
-    expect(data_repo.instance_variable_get("@data")).to eq({"first" => "hello", "second" => "hi"})
+    @data_repo.set("first", "hello")
+    @data_repo.set("second", "hi")
+    expect(@data_repo.instance_variable_get("@data")).to eq({"first" => "hello", "second" => "hi"})
   end
 
   it "finds data stored in the repo" do
-    data_repo = DataRepo.new
-    data_repo.set("first", "hello")
-    expect(data_repo.find("first")).to eq("hello")
+    @data_repo.set("first", "hello")
+    expect(@data_repo.find("first")).to eq("hello")
   end
 end
